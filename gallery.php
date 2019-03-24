12abb0d8b464bf968f332244dd5b6608a5761121
marcinwojcik987 <marcinwojcik987@gmail.com>
2019-03-24 23:02:31 +0100

    show all images in gallery

--- gallery.php
+++ gallery.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+require_once "dbConfig.php";
+
+if (!isset($_SESSION['logged_in'])){
+  header("Location:index.html");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,9 +21,6 @@
         <div class="heading">Image Gallery</div>
 <?php
 
-session_start();
-require_once "dbConfig.php";
-
 $mysql = new mysqli('localhost', 'root', '', 'codexworld') or die(mysqli_error($mysql));
 $query = $mysql->query("SELECT * FROM form_data ORDER BY id ASC") or 
           die($mysql->error);
@@ -22,15 +28,44 @@ $query = $mysql->query("SELECT * FROM form_data ORDER BY id ASC") or
 
 if (! empty($query)) {
     while ($row = $query->fetch_assoc()) {
-        ?>  
-            <div class="image">           
-                <img src="<?php echo "uploads/" . $row["file_name"] ; ?>" />            
+        ?> 
+        <section>
+        <div class="image">           
+                <img id="myImg" onclick="onClick(this)" src="<?php echo "uploads/" . $row["file_name"] ; ?>" />
+                <div id="myModal" class="modal">
+                <span class="close">&times;</span>
+                <img class="modal-content" id="img01">
+                <div id="caption"></div>
+                </div>            
             </div>
+        </section> 
+            
 <?php
     }
 }
 ?>
 </div>
-    
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+</script>    
 </body>
 </html>
