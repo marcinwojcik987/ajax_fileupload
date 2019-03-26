a788e365807e1d1cd978c811c43f9dcaaa28564e
marcinwojcik987 <marcinwojcik987@gmail.com>
2019-03-26 23:08:24 +0100

    name and email validation from front and backend

--- submit.php
+++ submit.php
@@ -15,14 +15,39 @@ if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_FILES['file']['
         }
     }
     
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+        $wszystko_OK = true;
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+       
+        //sprawdzenie czy nick sklada sie tylko z liter i cyfr bez polskich znakow
+        if (ctype_alnum($name)==false){
+            $wszystko_OK=false;            
+        }
+
+        $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);    
+        if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false)){
+            $wszystko_OK = false;            
+        }
+
+        if ($wszystko_OK==true) {
+            include_once 'dbConfig.php';    
+            //insert form data in the database
+            $insert = $db->query("INSERT form_data (name,email,file_name) VALUES ('".$name."','".$email."','".$uploadedFile."')");
+            
+            $db->close();
+        }
+
+    }
+
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
     
-    //include database configuration file
-    include_once 'dbConfig.php';
+    // //include database configuration file
+    // include_once 'dbConfig.php';
     
-    //insert form data in the database
-    $insert = $db->query("INSERT form_data (name,email,file_name) VALUES ('".$name."','".$email."','".$uploadedFile."')");
+    // //insert form data in the database
+    // $insert = $db->query("INSERT form_data (name,email,file_name) VALUES ('".$name."','".$email."','".$uploadedFile."')");
     
-    
+    // $db->close();
 }
